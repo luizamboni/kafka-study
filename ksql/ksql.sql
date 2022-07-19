c2bd167b9293f8b662435b9951ad775fb2b3e8a6
Luiz Zamboni <luizamboni2002@hotmail.com>
2022-07-18 21:34:18 -0300

    ADD basic streams from sql

--- ksql/ksql.sql
+++ ksql/ksql.sql
@@ -4,4 +4,26 @@ CREATE STREAM avro_example WITH (
     VALUE_FORMAT='AVRO'
 );
 
+-- to show the records of stream
+print test_arvro_schema;
+
+-- describe stream like a table, with columns and etc
 describe avro_example;
+describe extended avro_example;
+
+
+-- query from stream
+SELECT * 
+    FROM 
+        avro_example 
+    WHERE 
+        ROWKEY->NAME = 'Key' 
+    EMIT CHANGES;
+
+-- to get old messages
+SET 'auto.offset.reset'='earliest';
+--
+SELECT * 
+    FROM 
+        avro_example 
+    EMIT CHANGES;
